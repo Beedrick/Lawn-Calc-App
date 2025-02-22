c14b660b47ede1a632437b1d7e1a9519b5ec3925
Beedrick <pettric.m@yahoo.com>
2025-02-22 19:02:29 +1300

    changed calculator page name

--- kiwi-lawn-service/src/components/Navbar.js
+++ kiwi-lawn-service/src/components/Navbar.js
@@ -29,7 +29,7 @@ function Navbar() {
               <Link className="nav-link" to="/values">Values</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/calculator">Calculator</Link>
+              <Link className="nav-link" to="/calculator">Get Estimate</Link>
             </li>
           </ul>
         </div>
